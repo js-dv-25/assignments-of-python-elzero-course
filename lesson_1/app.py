a13701311f9456a6042e5e1345939e2fcce32d66
js-dv-25 <jsdev091@gmail.com>
2024-11-13 16:55:20 +0100

    Add files via upload

---
+++ lesson_1/app.py
@@ -0,0 +1,33 @@
+# one 
+
+# this is the first lesson in the course
+# i am now solving the assignments
+# wish me good luck !
+
+# two 
+
+name = "mohamed"
+age = "23"
+country = "morocco"
+
+# three
+
+
+print("\"Name: " + name + "\"")
+print("\"Age: " + age + "\"")
+print("\"Country: " + country + "\"")
+
+
+# four 
+
+# "Hello, My Name Is Osama And Iam 38 Years Old and I Live in Egypt."
+
+result = "\"Hello, My name is " + name + " and i am " + age + " years old and i live in " + country + ".\""
+
+print(result)
+
+# five 
+
+print(type(name))
+print(type(age))
+print(type(country))
